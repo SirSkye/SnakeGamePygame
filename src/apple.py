8aba50ba2c40c5afc579402eecf7b9a83ee835b1
SkyWalker <aisha278151@gmail.com>
2024-05-09 16:16:46 -0400

    Init apple class

---
+++ src/apple.py
@@ -0,0 +1,14 @@
+import pygame, sys
+from pygame.locals import *
+from random import randint
+from typing import Tuple
+
+class Apple():
+    def __init__(self):
+        #self.x = randint(0, tiles[0] - 1)
+        #self.y = randint(0, tiles[1] - 1)
+        pass
+    
+    def draw(self):
+        #fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
+        pass
