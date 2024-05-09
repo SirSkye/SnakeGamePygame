8586a8cb64209ee7edd66554c5164cd30a5d8789
SirSkye <foo@email.com>
2024-05-09 16:40:23 -0400

    Added logic for all body parts

--- src/snake.py
+++ src/snake.py
@@ -5,9 +5,9 @@ from utils import load_img
 
 class Snake():
     def __init__(self) -> None:
-        self.head = Vector2(4, 4)
-        self.body = [Vector2(4, 5), Vector2(4, 6)]
+        self.body = [Vector2(6, 4), Vector2(5, 4), Vector2(4, 4)]
         self.dir = Vector2(1, 0)
+        self.length = 3
 
         assets_path = r"..\assets"
         self.head_img = load_img(fr"{assets_path}\snake_head.png")
@@ -17,5 +17,11 @@ class Snake():
     
     def draw_snake(self, screen:pygame.Surface):
         TILE_SIZE = 50
-        base_rect = pygame.Rect(self.head.x * TILE_SIZE, self.head.y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
-        screen.blit(self.head_img, base_rect)
+        for index, body in enumerate(self.body):
+            base_rect = pygame.Rect(body.x * TILE_SIZE, body.y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
+            if index == 0:
+                screen.blit(self.head_img, base_rect)
+            elif index ==self.length - 1:
+                screen.blit(self.tail_img, base_rect)
+            else:
+                screen.blit(self.body_img, base_rect)
