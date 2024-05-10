e7aee8629b70907ed1f579ced31e158e4d123287
SirSkye <foo@email.com>
2024-05-10 18:53:27 -0400

    Added base screen and screen manager class

--- src/screens.py
+++ src/screens.py
@@ -1,5 +1,36 @@
 import pygame
 
 class BaseScreen:
-    def __init__(self, screen_manager) -> None:
-        self
+    def __init__(self, name, app):
+        self.name = name
+        self.app = app
+
+    def handle_events(self, event:pygame.event.Event) -> None:
+        pass
+
+    def update(self) -> None:
+        pass
+
+    def draw(self) -> None:
+        pass
+
+class ScreenManager:
+    def __init__(self, app) -> None:
+        self.app = app
+        self.screens = dict()
+        self.screen = None
+    
+    def add_screen(self, screen):
+        self.screens[screen.name] = screen
+    
+    def set_screen(self, name):
+        self.screen = self.screens[name]
+    
+    def update(self):
+        self.screen.update()
+    
+    def draw(self):
+        self.screen.draw()
+    
+    def handle_events(self, event):
+        self.screen.handle_events(event)
